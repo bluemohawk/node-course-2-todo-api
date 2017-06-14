295e1df5707156c8c30e9a796676b6e56957dfac
Everhard <you@example.com>
2017-06-14 23:24:40 +0200

    add get/todos/;id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,9 +30,39 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     resp.status(400).send(e);
   });
-  
+
 });
 
+//GET /todos/2322
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    console.log('invalid id');
+    return res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+
+    if (!todo) {
+      return res.status(404).send({text:'todo not found'});
+    }
+
+    res.send({todo});
+
+  }).catch((e)=> {
+    res.status(400).send({text:'!!'});
+  });
+
+
+    //findById
+      //success
+        //if todo
+        // if no todo
+      //error
+        //
+});
 
 
 app.listen(3000, () => {
