a505886c38714e6b7019917aa10436e0ade99de2
Everhard <you@example.com>
2017-06-14 16:48:30 +0200

    add get todos route

--- server/server.js
+++ server/server.js
@@ -9,19 +9,29 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res)=>{
+
   var todo = new Todo({
     text: req.body.text
   });
 
   todo.save().then(
     (doc) => {res.send(doc);},
-
     (e) => {res.status(400).send(e);}
   );
 
 });
 
-app.get('/')
+app.get('/todos', (req, res) => {
+
+  Todo.find().then(
+    (todos) => {
+      res.send({todos})
+  }, (e) => {
+    resp.status(400).send(e);
+  })
+});
+
+
 
 app.listen(3000, () => {
   console.log('Started listening');
