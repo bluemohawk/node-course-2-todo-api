27d8fb1cd7aa76ae0f1d7b4404bec4d129845cfe
Everhard <you@example.com>
2017-06-16 16:26:28 +0200

    add middleware

--- server/server.js
+++ server/server.js
@@ -6,13 +6,14 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-//Create a post user method
+//public route
 app.post('/users', (req, res)=>{
 
   var body = _.pick(req.body, ['email', 'password', 'tokens']);
@@ -25,6 +26,10 @@ app.post('/users', (req, res)=>{
   }).catch((e)=>{
       res.status(400).send(e);
     })
+});
+
+app.get('/user/me', authenticate, (req, res)=>{
+  res.send(req.user);
 });
 
 app.post('/todos', (req, res)=>{
