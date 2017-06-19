336151819028bf9d93fec1691595cc313d50a7c9
Everhard <you@example.com>
2017-06-19 14:48:19 +0200

    private and associated users

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-//public route
+//create a new account
 app.post('/users', (req, res)=>{
 
-  var body = _.pick(req.body, ['email', 'password', 'tokens']);
+  var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
   user.save().then(() => {
@@ -29,6 +29,7 @@ app.post('/users', (req, res)=>{
 
 });
 
+//logout
 app.delete('/users/me/token', authenticate, (req, res)=>{
   req.user.removeToken(req.token).then(()=>{
     res.status(200).send();
@@ -38,9 +39,10 @@ app.delete('/users/me/token', authenticate, (req, res)=>{
 
 });
 
+//login
 app.post('/users/login', (req, res)=>{
 
-  var credentials = _.pick(req.body, ['email', 'password', 'tokens']);
+  var credentials = _.pick(req.body, ['email', 'password']);
 
   User.findByCredentials(credentials.email, credentials.password).then((user)=>{
     return user.generateAuthToken();
@@ -56,9 +58,11 @@ app.get('/user/me', authenticate, (req, res)=>{
   res.send(req.user);
 });
 
-app.post('/todos', (req, res)=>{
-
+//create a new todo
+app.post('/todos', authenticate, (req, res)=>{
+  console.log({req});
   var todo = new Todo({
+    _creator: req.user._id,
     text: req.body.text
   });
 
@@ -69,9 +73,12 @@ app.post('/todos', (req, res)=>{
 
 });
 
-app.get('/todos', (req, res) => {
-
-  Todo.find().then(
+//get an existing todo
+app.get('/todos', authenticate, (req, res) => {
+console.log(req.user._id);
+  Todo.find({
+    _creator: req.user._id
+  }).then(
     (todos) => {res.send({todos});
   },
     (e)=>{resp.status(400).send(e);
@@ -79,7 +86,8 @@ app.get('/todos', (req, res) => {
 
 });
 
-app.get('/todos/:id', (req, res) => {
+//get a specific todo
+app.get('/todos/:id', authenticate, (req, res) => {
 
   var id = req.params.id;
 
@@ -88,7 +96,10 @@ app.get('/todos/:id', (req, res) => {
     return res.status(404).send();
   };
 
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
 
     if (!todo) {
       return res.status(404).send({text:'todo not found'});
@@ -102,7 +113,8 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.delete('/todos/:id', (req, res) => {
+//delete a specific todo
+app.delete('/todos/:id', authenticate, (req, res) => {
 
   var id = req.params.id;
 
@@ -110,7 +122,10 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(404).send({text: 'Id invalid'});
   };
 
-  Todo.findByIdAndRemove(id).then((todo)=>{
+  Todo.findOneAndRemove({
+    _id:id,
+    _creator:req.user._id
+  }).then((todo)=>{
 
     if(!todo) {
       return res.status(404).send({text: 'No todo found'})
@@ -123,7 +138,7 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
-app.patch('/todos/:id', (req, res) =>{
+app.patch('/todos/:id', authenticate, (req, res) =>{
 
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
@@ -139,7 +154,14 @@ app.patch('/todos/:id', (req, res) =>{
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+  Todo.findOneAndUpdate({
+    _id:id,
+    _creator:req.user._id
+  }, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo)=>{
     if (!todo) {
       return res.status(404).send({text:'todo to modified not found'});
     };
